5e20c187f564646b0a7ec52e6a889d105c6c7fe9
zacksleo <zacksleo@gmail.com>
2017-02-16 09:40:16 +0800

    add zh-TW language

---
+++ messages/zh-TW/default.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'Galleries' => '圖庫',
+    'Gallery manager' => '圖庫管理',
+    'Create' => '新建',
+    'Created' => '已創建',
+    'Update' => '更新',
+    'Updated' => '已更新',
+    'Delete' => '刪除',
+    'Reset' => '重置',
+    'View' => '查看',
+
+    'ID' => 'id',
+    'Name' => '名稱',
+    'Status' => '狀態',
+    'Caption' => '標題',
+    'Original' => '原始的',
+    'Update caption' => '更新標題',
+    'Remove image' => '刪除圖片',
+
+    'Attention! Save the model to work with the gallery' => '注意!保存模型以使用庫',
+
+    'Are you sure you want to delete this item?' => '確定要刪除此項嗎？ '
+];
