154bf328cc1a74933429cb6d3805f55bec27ad66
zacksleo <zacksleo@gmail.com>
2017-02-18 00:40:44 +0800

    remove kartik-vs grid

--- views/gallery/index.php
+++ views/gallery/index.php
@@ -1,7 +1,7 @@
 <?php
 
 use yii\helpers\Html;
-use kartik\grid\GridView;
+use yii\grid\GridView;
 use zacksleo\yii2\gallery\Module;
 
 $this->title = Module::t('default', 'Gallery manager');
@@ -14,43 +14,12 @@ $this->params['breadcrumbs'] = [
 <div class="gallery-index">
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'panel' => [
-            'type' => GridView::TYPE_DEFAULT,
-            'heading' => $this->title,
-        ],
-        'toolbar' => [
-            ['content'=>
-                Html::a('<i class="glyphicon glyphicon-plus"></i>', ['create'], [
-                    'title' => Module::t('default', 'Create'), 'class'=>'btn btn-success'
-                ]) . ' '.
-                Html::a('<i class="glyphicon glyphicon-repeat"></i>', [
-                    'index'
-                ], ['data-pjax' => 0, 'class' => 'btn btn-default', 'title' => Module::t('default', 'Reset')])
-            ],
-            '{toggleData}'
-        ],
-        'export' => false,
-        'pjax' => true,
-        'condensed' => true,
         'columns' => [
-            ['class' => 'kartik\grid\SerialColumn'],
-            [
-                'attribute' => 'id',
-                'width' => '75px'
-            ],
+            ['class' => 'yii\grid\SerialColumn'],
+            'id',
             'name',
-            [
-                'class' => 'kartik\grid\BooleanColumn',
-                'attribute' => 'status',
-                'vAlign' => 'middle',
-            ],
-//            'created',
-//            'updated',
-            [
-                'class' => 'kartik\grid\ActionColumn',
-                'vAlign' => 'middle',
-            ],
+            'status',
+            ['class' => 'yii\grid\ActionColumn'],
         ],
     ]); ?>
 </div>
